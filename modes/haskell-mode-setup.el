b4374a8b954bd4982e8a11b9b9420f4806824d0c
Badi' Abdul-Wahid <badi@Eru.erunet>
2008-09-29 00:57:34 -0700

    Haskell mode setup is aware of the darwin system type and adjusts the path to ghci to account for the MacPorts installation

--- modes/haskell-mode-setup.el
+++ modes/haskell-mode-setup.el
@@ -4,4 +4,10 @@
 (add-to-list 'load-path haskell-mode-root)
 (load (concat haskell-mode-root "/haskell-site-file.el"))
 
+<<<<<<< HEAD:modes/haskell-mode-setup.el
 (setq haskell-program-name "/opt/local/bin/ghci")
+=======
+(setq haskell-program-name (if system-type 'darwin
+							 "/opt/local/bin/ghci"
+							 "ghci"))
+>>>>>>> alpha:modes/haskell-mode-setup.el
