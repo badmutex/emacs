bb9a8f1a1b46bf99908a1083d5befcb9e0553a94
Badi' Abdul-Wahid <badi@Eru.erunet>
2008-09-29 00:59:54 -0700

    A few issues from merging should be fixed.

--- modes/haskell-mode-setup.el
+++ modes/haskell-mode-setup.el
@@ -4,10 +4,6 @@
 (add-to-list 'load-path haskell-mode-root)
 (load (concat haskell-mode-root "/haskell-site-file.el"))
 
-<<<<<<< HEAD:modes/haskell-mode-setup.el
-(setq haskell-program-name "/opt/local/bin/ghci")
-=======
 (setq haskell-program-name (if system-type 'darwin
-							 "/opt/local/bin/ghci"
-							 "ghci"))
->>>>>>> alpha:modes/haskell-mode-setup.el
+			     "/opt/local/bin/ghci"
+			     "ghci"))
