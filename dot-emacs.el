e98ef5d225a2ec85efbd57d89f45bdc19f3ee367
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2008-09-29 00:04:49 -0700

    Working on including auctex.

--- dot-emacs.el
+++ dot-emacs.el
@@ -4,6 +4,9 @@
 (add-to-list 'load-path emacs-root)
 (add-to-list 'load-path (concat emacs-root "/modes"))
 
+;;;; grab the relevent programming modes
 (require 'haskell-mode-setup)
+(require 'auctex-setup)
+
 (require 'keybindings)
 (require 'settings)
