0eabbf620f8760193beb92e8c7a8fc41d24da516
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2008-09-30 19:05:46 -0700

    Removed splash screen from startup.

--- dot-emacs.el
+++ dot-emacs.el
@@ -13,3 +13,18 @@
 
 ;;;; version control setup(s)
 (require 'git-setup)
+
+;;;; emacs self-customization
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t)
+ '(initial-scratch-message nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
