e743cab02b933f7933cb4cf800e960986419637c
Badi' Abdul-Wahid <badi@eru.erunet>
2008-11-30 01:12:23 -0800

    Don't track other unnessecary sources, just the binaries.

--- modes/clojure-setup.el
+++ modes/clojure-setup.el
@@ -6,7 +6,7 @@
 	     (home (if (eq system-type 'darwin) "/Users/badi/"
 				 (if (eq system-type 'gnu/linux) "/home/badi/"
 				   (if (eq system-type 'windows-nt) "/Users/badi/"))))
-	     (clojure-path (concat home "emacs/sw/clojure/target/"))
+	     (clojure-path (concat home "emacs/modes/clojure/"))
 	     (class-path-delimiter ";")
 	     (class-path (mapconcat (lambda (s) s)
 				    (list (concat clojure-path "clojure.jar"))
