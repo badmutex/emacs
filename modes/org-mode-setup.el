b6ea92cb7d42cbc087148e1c6f25045e456be62b
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-01-23 18:56:20 -0500

    [org] org agenda files, shortcuts added

--- modes/org-mode-setup.el
+++ modes/org-mode-setup.el
@@ -1,6 +1,23 @@
 
 
+(defvar org-root "~/org.git")
+
+(defvar agenda-file-names
+  (list "research.org"
+		"school.org"
+		"personal.org"))
+
 (setq auto-mode-alist (cons '("\\.org" . org-mode) auto-mode-alist))
+
+(setq org-agenda-files
+	  (map 'list (lambda (path)
+				   (concat org-root "/" path))
+		   agenda-file-names))
+
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+
+(setq org-log-done t)
 
 
 
