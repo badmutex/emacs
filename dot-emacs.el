467a83b8c889e305784a32f1cc86186c8f0837a4
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-05-02 16:33:35 -0400

    No more need to change faces since I adjusted colors in .Xdefaults

--- dot-emacs.el
+++ dot-emacs.el
@@ -21,17 +21,3 @@
 (require 'settings)
 
 (require 'my-stuff)
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(font-lock-builtin-face ((((class color) (min-colors 8)) (:foreground "yellow" :weight bold))))
- '(font-lock-function-name-face ((((class color) (min-colors 8)) (:foreground "red" :weight bold))))
- '(org-todo ((t (:foreground "magenta" :weight bold)))))
