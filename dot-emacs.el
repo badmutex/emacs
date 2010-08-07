d10e5921420b1f7051b67a39751fda1bdcb42ab4
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2010-08-07 17:17:29 -0400

    php mode

--- dot-emacs.el
+++ dot-emacs.el
@@ -4,8 +4,8 @@
 (add-to-list 'load-path emacs-root)
 (add-to-list 'load-path (concat emacs-root "/modes"))
 
-;;;; package manager
-(require 'elpa)
+;; ;;;; package manager
+;; (require 'elpa)
 
 ;;;; grab some usefull modes
 (require 'haskell-mode-setup)
@@ -13,9 +13,12 @@
 (require 'latex-setup)
 (require 'git-setup)
 (require 'elisp-setup)
+(require 'php-setup)
+
 (provide 'yasnippet-setup)
 (provide 'scala-setup)
 (provide 'fsharp-setup)
+
 
 (require 'keybindings)
 (require 'settings)
---
+++ modes/php-setup.el
@@ -0,0 +1,4 @@
+(autoload 'php-mode "php-mode.el" "Php mode." t)
+(setq auto-mode-alist (append '(("/*.\.php[345]?$" . php-mode)) auto-mode-alist))
+
+(provide 'php-setup)
