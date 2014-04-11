6f1e99d82fdba8b6f09af2a709db8f18c6d879f3
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2014-04-11 11:48:08 -0400

    add melpa to packages

--- packages-setup.el
+++ packages-setup.el
@@ -4,6 +4,8 @@
 (require 'package)
 ;; Add the original Emacs Lisp Package Archive
 (add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
              '("elpa" . "http://tromey.com/elpa/"))
 ;; Add the user-contributed repository
 (add-to-list 'package-archives
