2a0e6775f2bb87f8d047b8d01f314e83f3e70362
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2015-02-23 11:48:39 -0500

    add markdown mode

--- modes-setup.el
+++ modes-setup.el
@@ -159,6 +159,12 @@
 (add-to-list 'load-path (concat modes-root "/go-mode/"))
 (require 'go-mode)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Markdown mode
+(add-to-list 'load-path (concat modes-root "/markdown-mode/"))
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (provide 'modes-setup)
---
+++ modes/markdown-mode/update.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+mydir=$(dirname $0)
+cd $mydir
+wget \
+    http://jblevins.org/projects/markdown-mode/markdown-mode.el
